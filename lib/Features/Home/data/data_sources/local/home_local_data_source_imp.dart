a12b7a4524a0dd8ff0a18ca1112e691aedf93f21
yusufabdo20 <yusufa.fattah@gmail.com>
2024-03-25 13:19:34 +0200

    Add local data retrieval logic in HomeLocalDataSourceImp
    Add logic to fetch local books from Hive box in fetchFeaturedBooks method.

--- lib/Features/Home/data/data_sources/local/home_local_data_source_imp.dart
+++ lib/Features/Home/data/data_sources/local/home_local_data_source_imp.dart
@@ -1,16 +1,21 @@
 import 'package:bookly_app/Features/Home/domain/entities/book_entity.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'home_local_data_source.dart';
 
 class HomeLocalDataSourceImp extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
- 
+    var box = Hive.box<BookEntity>(AssetsData.kBookEntityBox);
+    List<BookEntity> localBooks = box.values.toList();
+    return localBooks;
   }
 
   @override
   List<BookEntity> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(AssetsData.kBookEntityBox);
+    List<BookEntity> localBooks = box.values.toList();
+    return localBooks;
   }
 }
