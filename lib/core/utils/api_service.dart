03de58356e1fc68117b2ae42090cc10895ca8317
yusufabdo20 <yusufa.fattah@gmail.com>
2024-03-25 11:57:05 +0200

    Refactor ApiService get method signature
    Add queryParameters and headers parameters to improve flexibility and customization
    of the get method in ApiService class.

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -4,9 +4,18 @@ class ApiService {
   final _baseUrl = 'https://www.googleapis.com/books/v1/';
   final Dio _dio;
   ApiService(this._dio);
-  Future<Map<String, dynamic>> get({required String endPoint}) async {
+
+  Future<Map<String, dynamic>> get({
+    required String endPoint,
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? headers,
+  }) async {
     print("In GET API SERVICE");
-    var response = await _dio.get('$_baseUrl$endPoint');
+    var response = await _dio.get('$_baseUrl$endPoint',
+        queryParameters: queryParameters,
+        options: Options(
+          headers: headers,
+        ));
     return response.data;
   }
 }
