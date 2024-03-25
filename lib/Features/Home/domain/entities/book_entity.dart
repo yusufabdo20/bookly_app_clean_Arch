0b1e9356a88ec517c8d37a305a2fe6fec1ceafb9
yusufabdo20 <yusufa.fattah@gmail.com>
2024-03-25 12:40:25 +0200

    Add Hive annotations and generated code for BookEntity
    Added Hive annotations and generated code for BookEntity class to enable serialization and deserialization using Hive.

--- lib/Features/Home/domain/entities/book_entity.dart
+++ lib/Features/Home/domain/entities/book_entity.dart
@@ -1,10 +1,19 @@
+import 'package:hive_flutter/adapters.dart';
+part'book_entity.g.dart' ; 
+@HiveType(typeId: 0)
 class BookEntity {
   /// data representation in UI .
+  @HiveField(0)
   final String? bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String? title;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final num? ratting;
 
   BookEntity({
