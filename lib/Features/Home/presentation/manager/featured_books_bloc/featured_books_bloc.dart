1fd2adc804d5e34c180c3db09ab1a373f207a93c
yusufabdo20 <yusufa.fattah@gmail.com>
2024-03-27 14:43:59 +0200

    Add home repo and fetch featured books use case to FeaturedBooksBloc
    Initialize home repo and fetch featured books use case in FeaturedBooksBloc
    constructor. Implement logic to fetch and emit featured books based on
    GetFeaturedBooksEvent.

--- lib/Features/Home/presentation/manager/featured_books_bloc/featured_books_bloc.dart
+++ lib/Features/Home/presentation/manager/featured_books_bloc/featured_books_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/domain/repos/home_repo.dart';
+import 'package:bookly_app/Features/Home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../domain/entities/book_entity.dart';
@@ -7,9 +9,21 @@ part 'featured_books_event.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksBloc extends Bloc<FeaturedBooksEvent, FeaturedBooksState> {
-  FeaturedBooksBloc() : super(FeaturedBooksInitial()) {
-    on<FeaturedBooksEvent>((event, emit) {
-      
+  FetchFeaturdBooksUseCase featurdBooksUseCase;
+  List<BookEntity> books = [];
+  FeaturedBooksBloc({required this.featurdBooksUseCase})
+      : super(FeaturedBooksInitial()) {
+    on<FeaturedBooksEvent>((event, emit) async {
+      if (event is GetFeaturedBooksEvent) {
+        emit(FeaturedBooksLoading());
+        var result = await featurdBooksUseCase.call();
+        result.fold(
+            (failure) => emit(FeaturedBooksFailure(error: failure.errMessage)),
+            (r) {
+          books = r;
+          emit(FeaturedBooksSuccess(books: books));
+        });
+      }
     });
   }
 }
